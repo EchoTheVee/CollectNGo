e2f745bf9440c63dead0a22276d20086ee4952dc
EchoTheVee <taikorodon@gmail.com>
2024-12-03 09:50:39 -0800

    Fixed Collectable Functionality

---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour
+{
+    public GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gm.collectablesGotten += 1;
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int collectablesGotten = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
